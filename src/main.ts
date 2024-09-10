63f50e7bcbcfa0de4485edc44499e053b1f897a7
Travis Burandt <travis.burandt@gmail.com>
2024-09-10 15:17:03 -0500

    logger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { CustomLogger } from './logger/custom-logger';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import * as cookieParser from 'cookie-parser';
+import { resolve } from 'path';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+async function bootstrap(logger: CustomLogger) {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, { logger });
+
+  app.useStaticAssets(resolve('./src/public'));
+  app.setBaseViewsDir(resolve('./src/views'));
+  app.setViewEngine('ejs');
+
+  app.use(cookieParser());
+
+  // so browsers can use api
+  app.enableCors({
+    origin: '*',
+  });
+
   await app.listen(3030);
 }
-bootstrap();
+
+// Create the CustomLogger
+const logger = new CustomLogger('Bootstrap');
+
+bootstrap(logger);
