9b54241ce14ee37ed17a99c07352f97aaa9b3a98
Travis Burandt <travis.burandt@gmail.com>
2024-09-10 12:43:59 -0500

    message queue client build

---
+++ client/dist/index.d.ts
@@ -0,0 +1,13 @@
+export declare class MessageQueueClient {
+    private axiosInstance;
+    private socket;
+    constructor(baseURL: string);
+    createExchange(name: string): Promise<any>;
+    bind(exchange: string, queue: string, routingKey: string): Promise<any>;
+    publish(exchange: string, routingKey: string, message: any): Promise<any>;
+    consume(exchange: string, queue: string): Promise<any>;
+    getQueueLength(exchange: string, queue: string): Promise<any>;
+    connectWebSocket(): Promise<void>;
+    subscribeToQueue(exchange: string, queue: string, callback: (message: any) => void): Promise<void>;
+    disconnectWebSocket(): void;
+}
---
+++ client/dist/index.js
@@ -0,0 +1,68 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.MessageQueueClient = void 0;
+const axios_1 = require("axios");
+const socket_io_client_1 = require("socket.io-client");
+class MessageQueueClient {
+    constructor(baseURL) {
+        this.socket = null;
+        this.axiosInstance = axios_1.default.create({
+            baseURL,
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+    }
+    async createExchange(name) {
+        const response = await this.axiosInstance.post('/message-broker/exchange', { name });
+        return response.data;
+    }
+    async bind(exchange, queue, routingKey) {
+        const response = await this.axiosInstance.post('/message-broker/bind', { exchange, queue, routingKey });
+        return response.data;
+    }
+    async publish(exchange, routingKey, message) {
+        const response = await this.axiosInstance.post('/message-broker/publish', { exchange, routingKey, message });
+        return response.data;
+    }
+    async consume(exchange, queue) {
+        const response = await this.axiosInstance.get(`/message-broker/consume/${exchange}/${queue}`);
+        return response.data;
+    }
+    async getQueueLength(exchange, queue) {
+        const response = await this.axiosInstance.get(`/message-broker/queue-length/${exchange}/${queue}`);
+        return response.data;
+    }
+    connectWebSocket() {
+        return new Promise((resolve) => {
+            this.socket = (0, socket_io_client_1.io)(this.axiosInstance.defaults.baseURL);
+            this.socket.on('connect', () => {
+                console.log('Connected to WebSocket server');
+                resolve();
+            });
+        });
+    }
+    subscribeToQueue(exchange, queue, callback) {
+        return new Promise((resolve) => {
+            if (!this.socket) {
+                throw new Error('WebSocket is not connected. Call connectWebSocket() first.');
+            }
+            this.socket.emit('subscribe', { exchange, queue }, (response) => {
+                console.log(`Subscription response for ${exchange}/${queue}:`, response);
+                this.socket.on('message', (data) => {
+                    if (data.exchange === exchange && data.queue === queue) {
+                        callback(data.message);
+                    }
+                });
+                resolve();
+            });
+        });
+    }
+    disconnectWebSocket() {
+        if (this.socket) {
+            this.socket.close();
+            this.socket = null;
+        }
+    }
+}
+exports.MessageQueueClient = MessageQueueClient;
